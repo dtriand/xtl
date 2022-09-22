b69fb5d7d6b251adc9330d5cab5a9f59c620e01b
dtriand <64985525+dtriand@users.noreply.github.com>
2022-09-22 18:31:13 +0200

    Added ImageMask class
    xtl.diffraction.images:ImageMask
    - Class for representation of mask for 2D diffraction images. Used for
      azimuthal integrations.
    - True = keep values; False = discard values
    - Methods for masking single pixels, rows, columns, rectangles, circles
      arbitrary polygons, or by intensity values
    
    xtl.diffraction.images:Image
    - Updates to ensure compatibility with ImageMask

--- src/xtl/diffraction/images/images.py
+++ src/xtl/diffraction/images/images.py
@@ -1,9 +1,11 @@
+import copy
 from functools import partial
 from math import floor
 from pathlib import Path
 
 import fabio
 import matplotlib.cm
+import matplotlib.path
 import matplotlib.pyplot as plt
 import numpy as np
 import pyFAI
@@ -17,6 +19,7 @@ class Image:
         self.fmt: str = None
         self.frame: int = None
         self.header_only: bool = False
+        self.mask: ImageMask = None
         self._fabio: fabio.fabioimage.FabioImage = None
         self._pyfai: pyFAI.AzimuthalIntegrator = None
 
@@ -24,6 +27,7 @@ class Image:
         self.cmap_bad_values = 'white'
         self.mask_color = (0, 1, 0)
         self.mask_alpha = 0.5
+        self.detector_image_origin = 'upper'
 
 
     def open(self, file: str or Path, frame: int = None):
@@ -33,6 +37,7 @@ class Image:
         self.frame = frame
         self._fabio = fabio.open(self.file, self.frame)
         self.fmt = self._fabio.classname
+        self.mask = self.make_mask()
 
 
     def openheader(self, file: str or Path):
@@ -58,8 +63,13 @@ class Image:
         self._pyfai = pyFAI.load(str(file))
 
 
-    def load_mask(self, array):
-        ...
+    @property
+    def beam_center(self):
+        return self._pyfai.poni2 / self._pyfai.pixel2, self._pyfai.poni1 / self._pyfai.pixel1
+
+    def make_mask(self):
+        self.mask = ImageMask(nx=self._fabio.shape[-2], ny=self._fabio.shape[-1], parent=self)
+        return self.mask
 
 
     def azimuthal_integration_cake(self, **kwargs):
@@ -72,7 +82,9 @@ class Image:
         npt_rad = kwargs.get('npt_rad', floor(max(pixels_from_center)))
         npt_azim = kwargs.get('npt_azim', 360)
         error_model = kwargs.get('error_model', 'poisson')
-        mask = kwargs.get('mask', None)  # self.mask
+        mask = kwargs.get('mask', self.mask)
+        if isinstance(mask, ImageMask):
+            mask = mask.data
         dummy = kwargs.get('dummy', None)
         unit = kwargs.get('unit', 'q_A^-1')
         if unit in ['2theta', 'tth', '2th']:
@@ -97,7 +109,11 @@ class Image:
         pixels_from_center = x_size - center[0], y_size - center[0]
         npt = kwargs.get('npt', floor(max(pixels_from_center)))
         error_model = kwargs.get('error_model', 'poisson')
-        mask = kwargs.get('mask', None)  # self.mask
+        mask = kwargs.get('mask', self.mask)
+        if mask is None:
+            mask = self.make_mask().data
+        elif isinstance(mask, ImageMask):
+            mask = mask.data
         dummy = kwargs.get('dummy', None)
         unit = kwargs.get('unit', 'q_A^-1')
         if unit in ['2theta', 'tth', '2th']:
@@ -123,16 +139,24 @@ class Image:
             norm = partial(matplotlib.colors.LogNorm, clip=False)
         vmin = kwargs.pop('vmin', None)
         vmax = kwargs.pop('vmax', None)
+        mask = kwargs.get('mask', self.mask)
+        if isinstance(mask, ImageMask):
+            mask = mask.data
+        overlay_mask = kwargs.pop('overlay_mask', None)
 
         cmap = matplotlib.cm.get_cmap(self.cmap)
         cmap.set_bad(color=self.cmap_bad_values, alpha=1.0)
 
-        # pos = ax.imshow(self.data, cmap=cmap, norm=norm(vmin=0, vmax=1e9))
-        pos = ax.imshow(self.data, cmap=cmap, norm=norm(vmin=vmin, vmax=vmax))
+        pos = ax.imshow(self.data, cmap=cmap, norm=norm(vmin=vmin, vmax=vmax), origin=self.detector_image_origin)
         if self._pyfai:
             # why is this in reverse?
             center_in_pixels = self._pyfai.poni2 / self._pyfai.pixel2, self._pyfai.poni1 / self._pyfai.pixel1
             ax.scatter(*center_in_pixels, marker='X', s=70, facecolors='red', edgecolors='white')
+        if overlay_mask:
+            mask_color = matplotlib.colors.to_rgba(self.mask_color)
+            mask_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('mask', [mask_color, (1, 1, 1, 0)], N=2)
+            ax.imshow(self.mask.data, cmap=mask_cmap, alpha=self.mask_alpha, origin=self.detector_image_origin,
+                      vmin=0, vmax=1)
 
         # fig.colorbar(pos, label='Intensity')
         ax.set_title(self.file.name)
@@ -146,7 +170,11 @@ class Image:
             norm = partial(matplotlib.colors.LogNorm, clip=False)
         vmin = kwargs.pop('vmin', None)
         vmax = kwargs.pop('vmax', None)
-        mask = kwargs.pop('mask', None)
+        mask = kwargs.get('mask', self.mask)
+        if mask is None:
+            mask = self.make_mask().data
+        elif isinstance(mask, ImageMask):
+            mask = mask.data
         overlay_mask = kwargs.pop('overlay_mask', None)
         xlabel = kwargs.pop('xlabel', 'Radial angle / 2\u03b8 (\u00b0)')
 
@@ -154,21 +182,19 @@ class Image:
         cmap.set_bad(color=self.cmap_bad_values, alpha=1.0)
 
         integrator = partial(self.azimuthal_integration_cake, error_model=None, **kwargs)
-        intensities, ttheta, chi = integrator(_data=self.data, mask=mask)
-        extent = (ttheta.min(), ttheta.max(), chi.min(), chi.max())
-        pos = ax.imshow(intensities, origin='lower', extent=extent, cmap=cmap, aspect='auto', interpolation='nearest',
-                        norm=norm(vmin=vmin, vmax=vmax))
+        intensities, ttheta, chi = integrator(_data=self.data, mask=~mask)
+        pos = ax.imshow(intensities, origin='lower', extent=(ttheta.min(), ttheta.max(), chi.min(), chi.max()),
+                        cmap=cmap, aspect='auto', interpolation='nearest', norm=norm(vmin=vmin, vmax=vmax))
 
         if overlay_mask:
-            intensities_masked = integrator(_data=~mask, mask=None)[0]
-            intensities_masked = np.clip(intensities_masked, 0, 1/1e9)
+            intensities_masked, ttheta, chi = integrator(_data=mask, mask=None)   # mask: True = keep, False = discard
+            intensities_masked = np.clip(intensities_masked, 0, 1/1e24)  # 0: discard, 1/1e24: keep
             mask_color = matplotlib.colors.to_rgba(self.mask_color)
-            cmap_mask = matplotlib.colors.LinearSegmentedColormap.from_list('mask', [mask_color, (1, 1, 1, 0)], N=2)
+            mask_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('mask', [mask_color, (1, 1, 1, 0)], N=2)
             ax.imshow(intensities_masked, origin='lower', extent=(ttheta.min(), ttheta.max(), chi.min(), chi.max()),
-                      cmap=cmap_mask, aspect='auto', interpolation='nearest', alpha=self.mask_alpha)
+                      cmap=mask_cmap, aspect='auto', interpolation='nearest', alpha=self.mask_alpha)
         ax.set_xlabel(xlabel)
         ax.set_ylabel('Azimuthal angle / \u03c7 (\u00b0)')
-
 
 
     def plot_1d(self, **kwargs):
@@ -179,13 +205,98 @@ class Image:
             norm = partial(matplotlib.colors.LogNorm, clip=False)
         vmin = kwargs.pop('vmin', None)
         vmax = kwargs.pop('vmax', None)
+        mask = kwargs.get('mask', self.mask)
+        if mask is None:
+            mask = self.make_mask().data
+        elif isinstance(mask, ImageMask):
+            mask = mask.data
         xlabel = kwargs.pop('xlabel', 'Radial angle / 2\u03b8 (\u00b0)')
         ylabel = kwargs.pop('ylabel', 'Intensity (arbitrary units)')
 
         cmap = matplotlib.cm.get_cmap(self.cmap)
         cmap.set_bad(color=self.cmap_bad_values, alpha=1.0)
 
-        ttheta, intensities = self.azimuthal_integration_1d(error_model=None, **kwargs)
+        ttheta, intensities = self.azimuthal_integration_1d(error_model=None, mask=~mask, **kwargs)
         pos = ax.plot(ttheta, intensities)
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
+
+
+class ImageMask:
+
+    def __init__(self, nx: int, ny: int, parent: Image = None):
+        self.nx = nx
+        self.ny = ny
+        self._data: np.ndarray
+        self._initialize_empty_mask()
+        self.parent: Image = parent
+
+
+    def _initialize_empty_mask(self):
+        self._data = np.ones((self.nx, self.ny), dtype=bool)  # array of True's
+
+
+    @property
+    def data(self):
+        return self._data
+
+
+    @property
+    def shape(self):
+        return self._data.shape
+
+    def mask_pixel(self, x: int, y: int):
+        self._data[y, x] = False
+
+    def mask_rows(self, i: int, j: int):
+        self._data[i:j+1, :] = False
+
+    def mask_cols(self, i: int, j: int):
+        self._data[:, i:j+1] = False
+
+    def mask_rectangle(self, pt1: tuple, pt2: tuple):
+        x1, y1 = pt1
+        x2, y2 = pt2
+        self._data[y1:y2+1, x1:x2+1] = False
+
+    def mask_polygon(self, pts: tuple):
+        vertices = [(y, x) for x, y in pts]
+        polygon = matplotlib.path.Path(vertices)
+
+        x, y = np.mgrid[0:self.nx, 0:self.ny]
+        coords = np.hstack((x.reshape(-1, 1), y.reshape(-1, 1)))
+        masked_points = ~polygon.contains_points(coords)
+
+        mask = masked_points.reshape(self.nx, self.ny)
+        self._data &= mask
+
+    def mask_circle(self, center: tuple, radius: float):
+        circle = matplotlib.path.Path.circle(center, radius)
+
+        x, y = np.mgrid[0:self.nx, 0:self.ny]
+        coords = np.hstack((x.reshape(-1, 1), y.reshape(-1, 1)))
+        masked_points = ~circle.contains_points(coords)
+
+        mask = masked_points.reshape(self.nx, self.ny)
+        self._data &= mask
+
+
+    def mask_intensity_greater_than(self, value: float):
+        if not self.parent:
+            raise Exception
+        self._data[self.parent.data > value] = False
+
+    def mask_intensity_less_than(self, value: float):
+        if not self.parent:
+            raise Exception
+        self._data[self.parent.data < value] = False
+
+
+    def __invert__(self):
+        new_mask = copy.deepcopy(self)
+        new_mask._data = ~new_mask._data
+        return new_mask
+
+    def invert(self):
+        self._data = ~self._data
+
