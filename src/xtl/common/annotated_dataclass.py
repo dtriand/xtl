ccd5d6cb13721c809c22ad23b50d79c68f6b24d4
dtriand <64985525+dtriand@users.noreply.github.com>
2024-11-17 16:51:11 +0100

    [WIP] New AnnotatedDataclass
    xtl.common.annotated_dataclasses:AnnotatedDataclass
    - New subclass for the stdlib dataclass that enables simple type
      checking, type casting and validation using custom fields with special
      values in their metadata attribute
    
    xtl.common.annotated_dataclasses:afield
    - Annotated dataclass field which stores all kwargs as metadata fields
    
    tests.common.tests_annotated_dataclasses
    - Tests for the above

---
+++ src/xtl/common/annotated_dataclass.py
@@ -0,0 +1,146 @@
+from dataclasses import dataclass, Field
+from dataclasses import field as _field
+
+
+@dataclass
+class AnnotatedDataclass:
+
+    def _derive_type(self, field_: Field):
+        t = field_.type
+        if hasattr(t, '__origin__'):
+            # For parsing generic types e.g. list[str]
+            return t.__origin__, t.__args__[0]
+        return t, None
+
+    def _typecheck_param(self, param: Field):
+        value = getattr(self, param.name)
+        if value is None:
+            return
+        # Derive the type and nested type of the field
+        t, nt = self._derive_type(param)
+        if not isinstance(value, t):
+            raise TypeError(f'Invalid parameter {param.name}\nExpected type {t}, got {type(value)}')
+        if nt:
+            for i, v in enumerate(value):
+                if not isinstance(v, nt):
+                    raise TypeError(f'Invalid parameter {param.name}[{i}]\nExpected type {nt}, got {type(v)}')
+        return
+
+    def _cast_param(self, param: Field):
+        value = getattr(self, param.name)
+        if value is None:
+            return
+        # Derive the type and nested type of the field
+        t, nt = self._derive_type(param)
+        if not isinstance(value, t):
+            return t(value)
+        if nt:
+            return t(nt(v) for v in value)
+        return value
+
+    def _validate_param(self, param: Field):
+        value = getattr(self, param.name)
+        if value is None:
+            return
+        if param.metadata.get('param_type') == 'compound':
+            for subparam in param.metadata.get('members', []):
+                if not hasattr(self, subparam):
+                    raise ValueError(f'Undefined member parameter {subparam} for {param.name}')
+        validator = param.metadata.get('validator', {})
+        if 'choice' in validator:
+            if value not in validator['choice']:
+                raise ValueError(f'Invalid value for {param.name}\nExpected one of {validator["choices"]}, got {value}')
+        if isinstance(value, str | list | tuple):
+            if 'len' in validator:
+                if len(value) != validator['len']:
+                    raise ValueError(f'Invalid value for {param.name}\nExpected length {validator["len"]}, '
+                                     f'got {len(value)}')
+        if isinstance(value, list | tuple):
+            if 'choices' in validator:
+                for i, v in enumerate(value):
+                    if v not in validator['choices']:
+                        raise ValueError(f'Invalid value for {param.name}[{i}]\nExpected one of {validator["choices"]}, '
+                                         f'got {v}')
+        if isinstance(value, float | int):
+            if 'gt' in validator:
+                if value <= validator['gt']:
+                    raise ValueError(f'Invalid value for {param.name}\nExpected value > {validator["gt"]}, got {value}')
+            if 'ge' in validator:
+                if value < validator['ge']:
+                    raise ValueError(f'Invalid value for {param.name}\nExpected value >= {validator["ge"]}, '
+                                     f'got {value}')
+            if 'lt' in validator:
+                if value >= validator['lt']:
+                    raise ValueError(f'Invalid value for {param.name}\nExpected value < {validator["lt"]}, got {value}')
+            if 'le' in validator:
+                if value > validator['le']:
+                    raise ValueError(f'Invalid value for {param.name}\nExpected value <= {validator["le"]}, '
+                                     f'got {value}')
+
+    def _typecheck_all(self):
+        for param in self.__dataclass_fields__.values():
+            if param.metadata.get('param_type', None) == '__internal':
+                continue
+            self._typecheck_param(param)
+
+    def _cast_all(self):
+        for param in self.__dataclass_fields__.values():
+            if param.metadata.get('param_type', None) == '__internal':
+                continue
+            try:
+                setattr(self, param.name, self._cast_param(param))
+            except ValueError:
+                raise ValueError(f'Invalid value for {param.name}: {getattr(self, param.name)}\n'
+                                 f'Expected type {param.type}, got {type(getattr(self, param.name))}')
+
+    def _validate_all(self):
+        for param in self.__dataclass_fields__.values():
+            if param.metadata.get('param_type', None) == '__internal':
+                continue
+            self._validate_param(param)
+
+    def __post_init__(self):
+        try:
+            self._typecheck_all()
+        except TypeError:
+            self._cast_all()
+        self._validate_all()
+
+
+def afield(**kwargs) -> Field:
+    """
+    Annotated dataclass field with additional metadata. Validation options can be passed as a `validator` dict, while
+    special formatting options as a `formatter` function.
+    """
+    kwargs['param_type'] = kwargs.get('param_type', 'standard')
+    default = kwargs.pop('default', None)
+    default_factory = kwargs.pop('default_factory', None)
+    if default_factory:
+        return _field(default_factory=default_factory, metadata=kwargs)
+    return _field(default=default, metadata=kwargs)
+
+
+def cfield(**kwargs) -> Field:
+    """
+    Compound annotated dataclass field with links to multiple members. The members of this field can be passes as a
+    `members` list. An extra `in_sync` flag can be toggled if all members should accept the parent's value.
+    """
+    kwargs['param_type'] = 'compound'
+    return afield(**kwargs)
+
+
+def pfield(**kwargs) -> Field:
+    """
+    Private annotated dataclass field for hidden internal attributes. This field will undergo type checking, type
+    casting and validation.
+    """
+    kwargs['param_type'] = 'private'
+    return afield(**kwargs)
+
+
+def _ifield(**kwargs) -> Field:
+    """
+    Hidden internal fields that will skip type checking, type casting and validaion.
+    """
+    kwargs['param_type'] = '__internal'
+    return afield(**kwargs)
---
+++ tests/common/test_annotated_dataclasses.py
@@ -0,0 +1,83 @@
+from wsgiref.validate import validator
+
+import pytest
+
+from dataclasses import dataclass
+
+from xtl.common.annotated_dataclass import AnnotatedDataclass, afield, cfield, pfield, _ifield
+
+
+class TestFields:
+
+    def test_afield(self):
+        # Default value
+        field = afield(param1='value1', default=42)
+        assert field.metadata == {'param1': 'value1', 'param_type': 'standard'}
+        assert field.default == 42
+
+        # Default factory
+        field = afield(param1='value1', default_factory=list)
+        assert field.metadata == {'param1': 'value1', 'param_type': 'standard'}
+        assert field.default_factory == list
+
+        # Multiple metadata values
+        field = afield(param1='value1', param2='value2')
+        assert field.metadata == {'param1': 'value1', 'param2': 'value2', 'param_type': 'standard'}
+        assert field.default is None
+
+    def test_cfield(self):
+        field = cfield(param1='value1')
+        assert field.metadata == {'param1': 'value1', 'param_type': 'compound'}
+
+    def test_pfield(self):
+        field = pfield(param1='value1')
+        assert field.metadata == {'param1': 'value1', 'param_type': 'private'}
+
+    def test_ifield(self):
+        field = _ifield(param1='value1')
+        assert field.metadata == {'param1': 'value1', 'param_type': '__internal'}
+
+
+class TestAnnotatedDataclass:
+
+    def test_post_init(self):
+        @dataclass
+        class TestClass(AnnotatedDataclass):
+            param1: int = afield(validator={'ge': 0})
+            param2: str = afield(validator={'len': 4})
+            param3: str = afield(validator={'choice': ['A', 'B', 'C']})
+            param4: list[int] = afield(validator={'len': 3})
+            param5: dict = afield()
+            param6: tuple = afield(validator={'choices': [1, 2, 3, 4]})
+            param7: set = afield()
+            param8: float = afield()
+            param9: complex = afield()
+            param10: str = pfield()
+            param11: int = _ifield()
+
+        # Valid initialization
+        t = TestClass(param1=42, param2='test', param3='A', param4=[1, 2, 3], param5={'a': 1, 'b': 2},
+                      param6=(1, 2, 3), param7={1, 2, 3}, param8=3.14, param9=3+4j)
+        assert t.param1 == 42
+        assert t.param2 == 'test'
+        assert t.param3 == 'A'
+        assert t.param4 == [1, 2, 3]
+        assert t.param5 == {'a': 1, 'b': 2}
+        assert t.param6 == (1, 2, 3)
+        assert t.param7 == {1, 2, 3}
+        assert t.param8 == 3.14
+        assert t.param9 == 3+4j
+
+        # Invalid initialization
+        with pytest.raises(ValueError):
+            TestClass(param1=-42)
+        with pytest.raises(ValueError):
+            TestClass(param2='testtesttest')
+        with pytest.raises(KeyError):
+            TestClass(param3='1')
+        with pytest.raises(ValueError):
+            TestClass(param4=[1, 2, 3, 4])
+        with pytest.raises(ValueError):
+            TestClass(param4=[1, 2, '3, 4'])
+        with pytest.raises(ValueError):
+            TestClass(param6=(1, 5))
