851c05638cad6552e69da04b570be06f02712ff4
dtriand <64985525+dtriand@users.noreply.github.com>
2025-03-04 10:53:56 +0100

    🚀 New `xtl.diffraction sum` CLI
    xtl.cli.diffraction.sum:cli_diffraction_sum
    - New CLI for performing summations or averaging of diffraction images

--- src/xtl/cli/diffraction/__init__.py
+++ src/xtl/cli/diffraction/__init__.py
@@ -1,11 +1,13 @@
 import typer
 
 from .geometry import app as geometry_app
+from .sum import app as sum_app
 from .integrate import app as integrate_app
 from .correlate import app as correlate_app
 
 
 app = typer.Typer(name='xtl.diffraction', help='Utilities for diffraction data')
 app.add_typer(geometry_app)
+app.add_typer(sum_app)
 app.add_typer(integrate_app)
 app.add_typer(correlate_app)
