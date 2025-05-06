802d2201dc66eec47384e7cbdf314fe31aa1d91d
dtriand <64985525+dtriand@users.noreply.github.com>
2025-05-06 23:33:45 +0200

    🚀 New ReflectionsData and ReflectionsCollection classes
    xtl.diffraction.reflections.reflections:ReflectionsData
    - New class for representing one column of some type of diffraction
      reflections data, e.g. intensities, phases, etc
    - This is a wrapper around reciprocalspaceship.DataSet, which in turn is
      a subclass of pandas.DataFrame
    - The class holds information about unit-cell, space group and whether
      the reflections are merged or not
    - The reflections are indexed with a three-column (H,K,L) pd.MultiIndex
    - .dtype and .mtz_type properties for retrieving the rs.MTZDtype and
      MTZ column type
    - .as_dtype() and .as_mtz_type() methods for changing the rs.MTZDtype
      and MTZ column type
    - Can be instantiated directly from Python/NumPy arrays, Pandas
      dataframes, gemmi.Mtz instances or rs.DataSeries
    - Alternatively, it can be instantiated from MTZ or CIF files by
      specifying the file and column name in the .from_mtz() and .from_cif()
      methods
    
    xtl.diffraction.reflections.reflections:ReflectionsCollection
    - New class for representing multiple columns of diffraction reflections
      data, e.g. intensities, phases, etc
    - This is a wrapper around reciprocalspaceship.DataSet, which in turn is
      a subclass of pandas.DataFrame
    - The class holds information about unit-cell, space group and whether
      the reflections are merged or not
    - The reflections are indexed with a three-column (H,K,L) pd.MultiIndex
    - .dtypes and .mtz_types properties for retrieving the rs.MTZDtype and
      MTZ column types
    - .as_dtype() and .as_mtz_type() methods for changing the rs.MTZDtype
      and MTZ column types
    - Can be instantiated directly from Python/NumPy arrays, Pandas
      dataframes, gemmi.Mtz instances or rs.DataSeries
    - Alternatively, it can be instantiated from MTZ or CIF files by
      specifying the file in the .from_mtz() and .from_cif() methods
    
    xtl.diffraction.reflections.mtz_types:mtz_types
    - Dictionary mapping of str -> rs.MTZDtype for all MTZ column types
    
    pyproject.toml & requirements.txt
    - Added reciprocalspaceship to the dependencies list

---
+++ src/xtl/diffraction/reflections/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['ReflectionsData', 'ReflectionsCollection', 'mtz_types']
+
+from .mtz_types import mtz_types
+from .reflections import ReflectionsData, ReflectionsCollection
+
---
+++ src/xtl/diffraction/reflections/reflections.py
@@ -0,0 +1,518 @@
+from pathlib import Path
+from typing import Optional, Iterable, Union
+
+import gemmi
+import reciprocalspaceship as rs
+from reciprocalspaceship.dtypes.base import MTZDtype
+import numpy as np
+import pandas as pd
+from pandas._typing import Dtype, ArrayLike
+
+
+from .mtz_types import mtz_types as _mtz_types
+
+
+class Reflection:
+    # Representation of a single reflection with one or more columns of data
+    ...
+
+
+class ReflectionsData:
+
+    def __init__(self,
+                 # pd.DataFrame arguments
+                 data: ArrayLike | Iterable | dict | pd.DataFrame | rs.DataSeries |
+                       gemmi.Mtz,
+                 index: Optional[ArrayLike | pd.Index] = None,
+                 columns: Optional[ArrayLike | pd.Index] = None,
+                 dtype: Optional[Dtype] = None,
+                 copy: bool = False,
+                 # Additional rs.DataSet arguments
+                 space_group: Optional[gemmi.SpaceGroup] = None,
+                 unit_cell: Optional[gemmi.UnitCell] = None,
+                 merged: Optional[bool] = None
+                 ):
+        # Extract space group and unit-cell from gemmi.Mtz if not provided
+        if isinstance(data, gemmi.Mtz):
+            space_group = space_group or data.spacegroup
+            unit_cell = unit_cell or data.cell
+        self._rs = rs.DataSet(data=data, index=index, columns=columns, dtype=dtype,
+                              copy=copy, spacegroup=space_group, cell=unit_cell,
+                              merged=merged)
+        if self._rs.shape[1] != 1:
+            raise ValueError(f'{self.__class__.__name__} must have 1 column, but got '
+                             f'{self._rs.shape[1]} columns')
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        """
+        The shape of the ReflectionsData
+        """
+        return self._rs.shape
+
+    @property
+    def ndim(self) -> int:
+        """
+        The number of dimensions
+        """
+        return self._rs.ndim
+
+    @property
+    def no_reflections(self) -> int:
+        """
+        The number of reflections in the ReflectionsData
+        """
+        return self.shape[0]
+
+    @property
+    def no_columns(self) -> int:
+        """
+        The number of columns in the ReflectionsData
+        """
+        return self.shape[1]
+
+    @property
+    def dtype(self) -> MTZDtype:
+        """
+        The data type of the ReflectionsData
+        """
+        return self._rs.dtypes.iloc[0]
+
+    def as_dtype(self, dtype: MTZDtype, copy: bool = False) -> None:
+        """
+        Convert the ReflectionsData to a different data type.
+        """
+        if copy:
+            self._rs = self._rs.copy()
+        self._rs = self._rs.astype(dtype)
+
+    @property
+    def mtz_type(self) -> str:
+        """
+        The MTZ type of the ReflectionsData
+        """
+        return self.dtype.mtztype
+
+    def as_mtz_type(self, mtz_type: str, copy: bool = False) -> None:
+        """
+        Convert the ReflectionsData to a different MTZ data type.
+        """
+        if mtz_type not in _mtz_types.keys():
+            raise ValueError(f'Invalid MTZ type {mtz_type!r}. '
+                             f'Choose one from: {", ".join(list(_mtz_types.keys()))}')
+        self.as_dtype(_mtz_types[mtz_type](), copy=copy)
+
+    @property
+    def column(self) -> pd.Index:
+        """
+        The column name of the ReflectionsData
+        """
+        return self._rs.columns
+
+    @property
+    def label(self) -> str:
+        """
+        The label name of the ReflectionsData
+        """
+        return self.column[0]
+
+    @property
+    def hkls(self) -> np.ndarray:
+        """
+        An array of the HKL indices
+        """
+        return self._rs.get_hkls()
+
+    @property
+    def values(self) -> rs.DataSeries:
+        """
+        The data of the ReflectionsData
+        """
+        return self._rs[self.label]
+
+    @property
+    def space_group(self) -> gemmi.SpaceGroup:
+        """
+        The space group of the ReflectionsData
+        """
+        return self._rs.spacegroup
+
+    @space_group.setter
+    def space_group(self, space_group: gemmi.SpaceGroup | str | int):
+        """
+        Set the space group of the ReflectionsData
+        """
+        self._rs.spacegroup = space_group
+
+    @property
+    def unit_cell(self) -> gemmi.UnitCell:
+        """
+        The unit-cell of the ReflectionsData
+        """
+        return self._rs.cell
+
+    @unit_cell.setter
+    def unit_cell(self, unit_cell: gemmi.UnitCell | np.ndarray | list[float | int] |
+                                   tuple[float | int]):
+        """
+        Set the unit-cell of the ReflectionsData
+        """
+        self._rs.cell = unit_cell
+
+    @property
+    def merged(self) -> bool:
+        """
+        The merged status of the ReflectionsData
+        """
+        return self._rs.merged
+
+    @merged.setter
+    def merged(self, merged: bool):
+        """
+        Set the merged status of the ReflectionsData
+        """
+        if not isinstance(merged, bool) and merged is not None:
+            raise TypeError(f'Must be a boolean, got {type(merged)}')
+        self._rs.merged = merged
+
+    def __getitem__(self, item):
+        # ToDo: More sophisticated implementation needed
+        return self._rs[self.label][item]
+
+    def __setitem__(self, key, value) -> None:
+        self._rs.loc[key, self.label] = value
+
+    @classmethod
+    def from_rs(cls, dataset: rs.DataSet, label: str,
+                space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                    tuple[float | int]] = None,
+                merged: Optional[bool] = None):
+        """
+        Create a ReflectionsData from a reciprocalspaceship DataSet.
+        """
+        if not isinstance(dataset, rs.DataSet):
+            raise TypeError(f'Expected reciprocalspaceship.DataSet, got {type(dataset)}')
+        if label not in dataset.columns:
+            raise ValueError(f'Label {label!r} not found in DataSet columns. '
+                             f'Choose one from: {", ".join(dataset.columns)}')
+        # Extract space group, unit-cell and merged from rs.DataSet if not provided
+        if space_group is None:
+            space_group = dataset.spacegroup
+        if unit_cell is None:
+            unit_cell = dataset.cell
+        if merged is None:
+            merged = dataset.merged
+        ic = dataset.columns.get_loc(label)
+        return cls(data=dataset[label], index=dataset.index,
+                   columns=dataset.columns[ic:ic+1], dtype=dataset.dtypes[label],
+                   copy=True, space_group=space_group, unit_cell=unit_cell,
+                   merged=merged)
+
+    @classmethod
+    def _from_rs_io(cls, file_type: str, file_reader: callable,
+                    file: Path | str, label: str,
+                    space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                    unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                    tuple[float | int]] = None,
+                    merged: Optional[bool] = None):
+        """
+        Create a ReflectionsData from a reciprocalspaceship reader function.
+        """
+        # Check if file exists
+        file = Path(file)
+        if not file.exists():
+            raise FileNotFoundError(f'{file_type} file {file} not found')
+        # Read the file
+        dataset: rs.DataSet = file_reader(str(file))
+        return cls.from_rs(dataset=dataset, label=label, space_group=space_group,
+                           unit_cell=unit_cell, merged=merged)
+
+    @classmethod
+    def from_mtz(cls, mtz_file: Path | str, label: str,
+                 space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                 unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                     tuple[float | int]] = None,
+                 merged: Optional[bool] = None):
+        """
+        Create a ReflectionsData from an MTZ file and a column label.
+        """
+        return cls._from_rs_io(file_type='MTZ', file_reader=rs.read_mtz,
+                                file=mtz_file, label=label, space_group=space_group,
+                                unit_cell=unit_cell, merged=merged)
+
+    @classmethod
+    def from_cif(cls, cif_file: Path | str, label: str,
+                 space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                 unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                     tuple[float | int]] = None,
+                 merged: Optional[bool] = None):
+        """
+        Create a ReflectionsData from an CIF file and a column label.
+        """
+        return cls._from_rs_io(file_type='CIF', file_reader=rs.read_cif,
+                                file=cif_file, label=label, space_group=space_group,
+                                unit_cell=unit_cell, merged=merged)
+
+
+
+class ReflectionsCollection:
+
+    def __init__(self,
+                 # pd.DataFrame arguments
+                 data: ArrayLike | Iterable | dict | pd.DataFrame | rs.DataSet |
+                       gemmi.Mtz,
+                 index: Optional[ArrayLike | pd.Index] = None,
+                 columns: Optional[ArrayLike | pd.Index] = None,
+                 dtypes: Optional[Iterable | pd.Index | pd.Series | dict[str, Dtype]] = None,
+                 copy: bool = False,
+                 # Additional rs.DataSet arguments
+                 space_group: Optional[gemmi.SpaceGroup] = None,
+                 unit_cell: Optional[gemmi.UnitCell] = None,
+                 merged: Optional[bool] = None
+                 ):
+        # Extract space group and unit-cell from gemmi.Mtz if not provided
+        if isinstance(data, gemmi.Mtz):
+            space_group = space_group or data.spacegroup
+            unit_cell = unit_cell or data.cell
+        self._rs = rs.DataSet(data=data, index=index, columns=columns, dtype=None,
+                              copy=copy, spacegroup=space_group, cell=unit_cell,
+                              merged=merged)
+
+        # Set dtypes
+        if dtypes is not None:
+            self.as_dtype(dtypes)
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        """
+        The shape of the ReflectionsCollection
+        """
+        return self._rs.shape
+
+    @property
+    def ndim(self) -> int:
+        """
+        The number of dimensions
+        """
+        return self._rs.ndim
+
+    @property
+    def no_reflections(self) -> int:
+        """
+        The number of reflections in the ReflectionsCollection
+        """
+        return self.shape[0]
+
+    @property
+    def no_columns(self) -> int:
+        """
+        The number of columns in the ReflectionsCollection
+        """
+        return self.shape[1]
+
+    @property
+    def dtypes(self) -> pd.Series:
+        """
+        The data types of the ReflectionsCollection
+        """
+        return self._rs.dtypes
+
+    def as_dtype(self, dtypes: Iterable | pd.Index | dict[str, Dtype], copy: bool = False) -> None:
+        if isinstance(dtypes, dict):
+            if len(dtypes.keys()) != self.no_columns:
+                raise ValueError(f'Expected {self.no_columns} dtypes, '
+                                 f'got {len(dtypes.keys())}')
+        elif isinstance(dtypes, Iterable | pd.Index):
+            if len(dtypes) != self.no_columns:
+                raise ValueError(f'Expected {self.no_columns} dtypes, got {len(dtypes)}')
+            dtypes = {col: dt for col, dt in zip(self.columns, dtypes)}
+        else:
+            raise TypeError(f'Expected dict or array-like, got {type(dtypes)}')
+        if copy:
+            self._rs = self._rs.copy()
+        self._rs = self._rs.astype(dtypes)
+
+    @property
+    def mtz_types(self) -> tuple[str, ...]:
+        """
+        The MTZ types of the ReflectionsCollection
+        """
+        return tuple([dt.mtztype for dt in self.dtypes.to_list()])
+
+    def as_mtz_types(self, mtz_types: Iterable[str], copy: bool = False) -> None:
+        """
+        Convert the ReflectionsCollection to a different MTZ data type.
+        """
+        if not isinstance(mtz_types, Iterable):
+            raise TypeError(f'Expected iterable, got {type(mtz_types)}')
+        if len(mtz_types) != self.no_columns:
+            raise ValueError(f'Expected {self.no_columns} MTZ types, '
+                             f'got {len(mtz_types)}')
+        dtypes = []
+        for i, mtz_type in enumerate(mtz_types):
+            if mtz_type not in _mtz_types.keys():
+                raise ValueError(f'Invalid MTZ type {mtz_type!r} for item {i}. '
+                                 f'Choose one from: {", ".join(list(_mtz_types.keys()))}')
+            dtypes.append(_mtz_types[mtz_type]())
+        self.as_dtype(dtypes=dtypes, copy=copy)
+
+    @property
+    def columns(self) -> pd.Index:
+        """
+        The column names of the ReflectionsCollection
+        """
+        return self._rs.columns
+
+    @property
+    def labels(self) -> tuple[str, ...]:
+        """
+        The labels of the ReflectionsCollection
+        """
+        return tuple(self.columns.to_list())
+
+    @property
+    def hkls(self) -> np.ndarray:
+        """
+        An array of the HKL indices
+        """
+        return self._rs.get_hkls()
+
+    @property
+    def values(self) -> rs.DataSet:
+        """
+        The data of the ReflectionsCollection
+        """
+        return self._rs
+
+    @property
+    def space_group(self) -> gemmi.SpaceGroup:
+        """
+        The space group of the ReflectionsCollection
+        """
+        return self._rs.spacegroup
+
+    @space_group.setter
+    def space_group(self, space_group: gemmi.SpaceGroup | str | int):
+        """
+        Set the space group of the ReflectionsCollection
+        """
+        self._rs.spacegroup = space_group
+
+    @property
+    def unit_cell(self) -> gemmi.UnitCell:
+        """
+        The unit-cell of the ReflectionsCollection
+        """
+        return self._rs.cell
+
+    @unit_cell.setter
+    def unit_cell(self, unit_cell: gemmi.UnitCell | np.ndarray | list[float | int] |
+                                   tuple[float | int]):
+        """
+        Set the unit-cell of the ReflectionsCollection
+        """
+        self._rs.cell = unit_cell
+
+    @property
+    def merged(self) -> bool:
+        """
+        The merged status of the ReflectionsCollection
+        """
+        return self._rs.merged
+
+    @merged.setter
+    def merged(self, merged: bool):
+        """
+        Set the merged status of the ReflectionsCollection
+        """
+        if not isinstance(merged, bool) and merged is not None:
+            raise TypeError(f'Must be a boolean, got {type(merged)}')
+        self._rs.merged = merged
+
+    # ToDo: Implement __getitem__ and __setitem__ for multiple columns
+    # def __getitem__(self, item):
+    #     return self._rs[self.label][item]
+    #
+    # def __setitem__(self, key, value) -> None:
+    #     self._rs.loc[key, self.label] = value
+
+    @classmethod
+    def from_rs(cls, dataset: rs.DataSet, labels: Optional[Iterable[str]] = None,
+                space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                    tuple[float | int]] = None,
+                merged: Optional[bool] = None):
+        """
+        Create a ReflectionsCollection from a reciprocalspaceship DataSet.
+        """
+        if not isinstance(dataset, rs.DataSet):
+            raise TypeError(f'Expected reciprocalspaceship.DataSet, got {type(dataset)}')
+        # Extract space group, unit-cell and merged from rs.DataSet if not provided
+        if space_group is None:
+            space_group = dataset.spacegroup
+        if unit_cell is None:
+            unit_cell = dataset.cell
+        if merged is None:
+            merged = dataset.merged
+
+        if labels is not None:
+            # Check if labels are present in dataset
+            if not isinstance(labels, Iterable):
+                raise TypeError(f'Expected iterable, got {type(labels)}')
+            for i, label in enumerate(labels):
+                if label not in dataset.columns:
+                    raise ValueError(f'Label[{i}] {label!r} not found in DataSet columns. '
+                                     f'Choose one from: {", ".join(dataset.columns)}')
+
+            # Filter dataset to only include the labels
+            dataset = dataset[labels]
+
+        return cls(data=dataset, index=dataset.index, columns=dataset.columns,
+                   dtypes=dataset.dtypes, copy=True, space_group=space_group,
+                   unit_cell=unit_cell, merged=merged)
+
+    @classmethod
+    def _from_rs_io(cls, file_type: str, file_reader: callable,
+                    file: Path | str, labels: Optional[Iterable[str]] = None,
+                    space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                    unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                    tuple[float | int]] = None,
+                    merged: Optional[bool] = None):
+        """
+        Create a ReflectionsCollection from a reciprocalspaceship reader function.
+        """
+        # Check if file exists
+        file = Path(file)
+        if not file.exists():
+            raise FileNotFoundError(f'{file_type} file {file} not found')
+        # Read the file
+        dataset: rs.DataSet = file_reader(str(file))
+        return cls.from_rs(dataset=dataset, labels=labels, space_group=space_group,
+                           unit_cell=unit_cell, merged=merged)
+
+    @classmethod
+    def from_mtz(cls, mtz_file: Path | str, labels: Optional[Iterable[str]] = None,
+                 space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                 unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                     tuple[float | int]] = None,
+                 merged: Optional[bool] = None):
+        """
+        Create a ReflectionsCollection from an MTZ file and a column label.
+        """
+        return cls._from_rs_io(file_type='MTZ', file_reader=rs.read_mtz,
+                                file=mtz_file, labels=labels, space_group=space_group,
+                                unit_cell=unit_cell, merged=merged)
+
+    @classmethod
+    def from_cif(cls, cif_file: Path | str, labels: Optional[Iterable[str]] = None,
+                 space_group: Optional[gemmi.SpaceGroup | str | int] = None,
+                 unit_cell: Optional[gemmi.UnitCell | np.ndarray | list[float | int] |
+                                     tuple[float | int]] = None,
+                 merged: Optional[bool] = None):
+        """
+        Create a ReflectionsCollection from an CIF file and a column label.
+        """
+        return cls._from_rs_io(file_type='CIF', file_reader=rs.read_cif,
+                                file=cif_file, labels=labels, space_group=space_group,
+                                unit_cell=unit_cell, merged=merged)
